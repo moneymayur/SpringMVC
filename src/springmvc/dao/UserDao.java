4b62ec7f3744a6c05bd358507e110d18c25d85d5
Mayur Mathur <moneymayur@users.noreply.github.com>
2017-09-12 00:19:42 +0530

    Add files via upload

---
+++ src/springmvc/dao/UserDao.java
@@ -0,0 +1,8 @@
+package springmvc.dao;
+
+import jbr.springmvc.model.Login;
+import jbr.springmvc.model.User;
+public interface UserDao {
+  void register(User user);
+  User validateUser(Login login);
+}
